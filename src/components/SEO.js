824f854e0fa58825955322bddb2bb96648060ac4
raniellavllrm <vraniella@gmail.com>
2021-11-08 20:27:00 +0800

    helmet & snap installed

---
+++ src/components/SEO.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Helmet } from "react-helmet"
+
+
+export const SEO = ({ title, description, meta = [] }) => {
+    return (
+        <Helmet title={title}
+            htmlAttributes={{ lang: "en" }}
+            meta={[
+                {
+                    name: `description`,
+                    content: description,
+                },
+            ]}
+        />
+    )
+}
