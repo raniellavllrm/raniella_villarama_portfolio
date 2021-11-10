3e8a9fa1c4b206e7c63b5dd6be0c83d6d1c6b35a
raniellavllrm <vraniella@gmail.com>
2021-11-10 11:46:21 +0800

    Update SEO.js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,7 +6,10 @@ const SEO = ({ title, description }) => {
     return (
         <HelmetProvider>
             <Helmet>
-                <html lang="en" />
+                <head>
+                    <meta name="google-site-verification" content="hLoUkG7zEdTk7YRyYRc8SuX_agsu4T5qmzYvXAyIg3A" />
+                    <html lang="en" />
+                </head>
                 <title>{title}</title>
                 <meta name="description" content={description} />
             </Helmet>
